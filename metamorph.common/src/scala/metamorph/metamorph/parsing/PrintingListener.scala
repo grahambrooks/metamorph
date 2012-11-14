64f44aabce5525d1200b372640068926caf94a18
Graham Brooks <graham@grahambrooks.com>
2012-11-14 14:59:05 -0500

    Migrated printing listener from Java to Scala

---
+++ metamorph.common/src/scala/metamorph/metamorph/parsing/PrintingListener.scala
@@ -0,0 +1,56 @@
+package metamorph.metamorph.parsing
+
+import org.antlr.v4.runtime.misc.Utils
+import scala.StringBuilder
+import org.antlr.v4.runtime.tree.{Tree, TerminalNode, ErrorNode, RuleNode}
+import org.antlr.v4.runtime.{Parser, Token}
+
+object PrintingListener {
+  def toStringTree(t: Tree, recognizer: Parser): String = toStringTree(t, if (recognizer != null) recognizer.getRuleNames else null)
+
+  def toStringTree(t: Tree, ruleNames: Array[String]): String = {
+
+    if (t.getChildCount == 0) return Utils.escapeWhitespace(getNodeText(t, ruleNames), false)
+
+    val buf: StringBuilder = new StringBuilder
+    buf.append("(")
+
+    buf.append(Utils.escapeWhitespace(getNodeText(t, ruleNames), false))
+    buf.append(' ')
+
+    for (i <- 0 to t.getChildCount()) {
+      if (i > 0) buf.append(' ')
+      buf.append(toStringTree(t.getChild(i), ruleNames))
+    }
+
+    buf.append(")")
+    buf.toString
+  }
+
+  def getNodeText(t: Tree, recognizer: Parser): String = getNodeText(t, if (recognizer != null) recognizer.getRuleNames else null)
+
+  def getNodeText(t: Tree, ruleNames: Array[String]): String = {
+    if (ruleNames != null) {
+      if (t.isInstanceOf[RuleNode]) {
+        val ruleIndex = (t.asInstanceOf[RuleNode]).getRuleContext.getRuleIndex
+        return ruleNames(ruleIndex)
+      } else if (t.isInstanceOf[ErrorNode[_ <: Token]]) {
+        return t.toString
+      } else if (t.isInstanceOf[TerminalNode[_ <: Token]]) {
+        val symbol = (t.asInstanceOf[TerminalNode[_]]).getSymbol
+
+        if (symbol.isInstanceOf[Token]) {
+          return (symbol.asInstanceOf[Token]).getText
+        }
+      }
+    }
+
+
+    val payload = t.getPayload
+    if (payload.isInstanceOf[Token]) {
+      (payload.asInstanceOf[Token]).getText
+    } else {
+      t.getPayload.toString
+    }
+  }
+}
--- metamorph.java.parser/src/scala/metamorph/java/DefaultJavaFileProcessor.scala
+++ metamorph.java.parser/src/scala/metamorph/java/DefaultJavaFileProcessor.scala
@@ -1,16 +1,14 @@
 package metamorph.java
 
 import _root_.java.io.IOException
-import metamorph.parsing.PrintingListener
 import org.antlr.v4.runtime.{CommonTokenStream, ANTLRFileStream, ANTLRInputStream}
+import metamorph.metamorph.parsing.PrintingListener
 
 final class DefaultJavaFileProcessor extends JavaFileProcessor {
 
   def processFile(filename: String) {
-    var input: ANTLRInputStream = null
     try {
-      input = new ANTLRFileStream(filename)
-      return sourceToAst(input)
+      sourceToAst(new ANTLRFileStream(filename))
     }
     catch {
       case e: IOException => {
@@ -23,12 +21,12 @@ final class DefaultJavaFileProcessor extends JavaFileProcessor {
     val parser = new JavaParser(new CommonTokenStream(new JavaLexer(input)))
     val tree = parser.compilationUnit
 
-    return PrintingListener.toStringTree(tree, parser)
+    PrintingListener.toStringTree(tree, parser)
   }
 
   def visitSource[T](input: ANTLRInputStream, visitor: JavaBaseVisitor[T]): T = {
     val parser = new JavaParser(new CommonTokenStream(new JavaLexer(input)))
 
-    return visitor.visit(parser.compilationUnit())
+    visitor.visit(parser.compilationUnit())
   }
 }
