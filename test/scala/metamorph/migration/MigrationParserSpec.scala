3681cbffad983ca3b8f9562f75438554ca899229
Graham Brooks <graham@grahambrooks.com>
2013-02-17 17:54:52 -0500

    Started work on parsing migrations from an external source

---
+++ test/scala/metamorph/migration/MigrationParserSpec.scala
@@ -0,0 +1,15 @@
+package metamorph.migration
+
+import org.scalatest.FunSpec
+import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream}
+
+class MigrationParserSpec extends FunSpec {
+  describe("Migration Parser") {
+    it("recognises rename") {
+      val migrationText = "migration rename com.some.class to com.some.other.class;"
+
+      val parser = new MigrationParser(new CommonTokenStream(new MigrationLexer(new ANTLRInputStream(migrationText))))
+      parser.migrationFile()
+    }
+  }
+}
